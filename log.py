57a785956a99b0939839242b1a75234ea9a3de40
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-04 00:29:48 -0300

    Salvar em UTF8 e variaveis do log

--- log.py
+++ log.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 from os import path
 
-RAIZ = 'logs'
+raiz = 'logs'
+extensao = 'txt'
 
 def salvar_log(mensagem:str) -> None:
     '''Salva o log em um arquivo TXT.'''
-    mensagem.capitalize()
+    mensagem = mensagem.capitalize()
     if not mensagem.endswith('.'):
         mensagem += '.'
     agora = datetime.now()
     data = agora.strftime('%Y-%m-%d')
     tempo = agora.strftime('%H : %M : %S')
-    nome_arquivo = data + '.txt'
-    caminho_arquivo = path.join(path.basename(RAIZ), path.basename(nome_arquivo))
-    with open(caminho_arquivo, 'a') as file:
+    nome_arquivo = data + '.' + extensao
+    caminho_arquivo = path.join(path.basename(raiz), path.basename(nome_arquivo))
+    with open(caminho_arquivo, 'a', encoding = 'UTF-8') as file:
         file.write(f'{tempo} -> {mensagem}\n')

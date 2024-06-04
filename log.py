2b4e80a264d12ae02c21c154d1444d8534b0b753
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-04 01:12:36 -0300

    Melhoria de config

--- log.py
+++ log.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from os import path
 
-raiz = 'logs'
-extensao = 'txt'
+from config import RAIZ_LOG, EXTENSAO_LOG
 
 def salvar_log(mensagem:str) -> None:
     '''Salva o log em um arquivo TXT.'''
@@ -12,7 +11,7 @@ def salvar_log(mensagem:str) -> None:
     agora = datetime.now()
     data = agora.strftime('%Y-%m-%d')
     tempo = agora.strftime('%H : %M : %S')
-    nome_arquivo = data + '.' + extensao
-    caminho_arquivo = path.join(path.basename(raiz), path.basename(nome_arquivo))
+    nome_arquivo = data + '.' + EXTENSAO_LOG
+    caminho_arquivo = path.join(path.basename(RAIZ_LOG), path.basename(nome_arquivo))
     with open(caminho_arquivo, 'a', encoding = 'UTF-8') as file:
         file.write(f'{tempo} -> {mensagem}\n')

e8a79ed86ae50626e2a61db6787700a9f8dc8653
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-07 17:06:04 -0300

    função log_erro

--- log.py
+++ log.py
@@ -3,7 +3,7 @@ from os import path
 
 from config import LOG_ROOT, LOG_EXTENSION
 
-def log(text:str, user:str = '') -> None:
+def log(text:str, username:str = '') -> None:
     '''Salva o log em um arquivo TXT.'''
     words = text.split(' ')
     if '[ERRO]' in words:
@@ -20,9 +20,9 @@ def log(text:str, user:str = '') -> None:
     file_name = date + '.' + LOG_EXTENSION
     file_path = path.join(path.basename(LOG_ROOT), path.basename(file_name))
     with open(file_path, 'a', encoding = 'UTF-8') as file:
-        file.write(f'{time} ({user}) -> {text}\n')
+        file.write(f'{time} ({username}) -> {text}\n')
 
-def log_erro(text:str, user:str = '') -> None:
+def log_erro(text:str, username:str = '') -> None:
     '''Salva o log de erro em um arquivo TXT.'''
     text = f'[ERRO] {text}'
-    log(text, user)
+    log(text, username)

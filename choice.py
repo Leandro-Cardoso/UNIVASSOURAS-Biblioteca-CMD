722e04578b4a7cf3ec401fd9cbca7bd1377b43c3
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-07 19:03:42 -0300

    Exibicao de erro

--- choice.py
+++ choice.py
@@ -1,6 +1,6 @@
 from log import log, log_erro
 
-def make_choice(options:list, username:str = '') -> int:
+def make_choice(options:list, username:str = '') -> int|str:
     choiced = input('\nOPÇÃO: ')
     try:
         choiced = int(choiced)
@@ -8,18 +8,10 @@ def make_choice(options:list, username:str = '') -> int:
         if 0 < choiced <= n:
             option = str(options[choiced - 1]).title()
             log(f'acessou a opção {choiced} - {option}', username)
-            return choiced, None
+            return choiced
         else:
             raise
     except:
         erro = f'opção {choiced} é inválida'
         log_erro(erro, username)
-        return -1, erro
-
-if __name__ == '__main__':
-    options = [
-        'sair',
-        'avançar'
-    ]
-    username = 'Leandro'
-    make_choice(options, username)
+        return erro
--- main2.py
+++ main2.py
@@ -3,13 +3,17 @@ from choice import make_choice
 from log import log
 
 def exit(username:str) -> None:
+    # SCREEN:
     title = 'biblioteca'
     infos = f'obrigado {username} por utilizar nosso sistema. volte sempre'
     draw_screen(title, infos)
     print()
 
 def login(user:dict) -> dict:
-    pass
+    # SCREEN:
+    title = 'login'
+    erro = None
+    return user
 
 def main_menu(user:dict) -> None:
     # SCREEN:
@@ -45,13 +49,21 @@ def main_menu(user:dict) -> None:
         draw_screen(title, infos, options, erro)
         # CHOICE:
         if user:
-            choiced, erro = make_choice(options, username)
+            choiced = make_choice(options, username)
         else:
-            choiced, erro = make_choice(options)
+            choiced = make_choice(options)
+        # ERRO:
+        if isinstance(choiced, str):
+            erro = choiced
+        else:
+            erro = None
+        # OPTIONS:
         if choiced == 1:
             log('saiu do sistema', username)
             exit(username)
             break
+        if choiced == 2:
+            login(user)
 
 def main() -> None:
     user = None

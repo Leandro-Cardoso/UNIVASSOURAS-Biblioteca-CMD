5830514c2f8a20b322846cd305329d75f8958be6
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-09 17:51:42 -0300

    Funcao para adicionar livro

--- menu/manage_books.py
+++ menu/manage_books.py
@@ -1,5 +1,92 @@
 from screen import draw_screen
 from choice import make_choice
+from book import get_book, add_book
+from log import log, log_erro
+
+def add_books(user:dict) -> None:
+    username = user['username']
+    # SCREEN:
+    title = 'adicionar livros'
+    infos = 'digite as informações abaixo pedidas e necessárias para adicionar um novo livro ou adicionar mais unidades'
+    erro = None
+    # TITLE:
+    draw_screen(title, infos, erro = erro)
+    booktitle = input('\n LIVRO: ')
+    booktitle = booktitle.title()
+    book = get_book(booktitle)
+    if isinstance(book, dict):
+        while True:
+            draw_screen(title, infos, erro = erro, dictionarys = book)
+            quantity = input('\n QUANTIDADE: ')
+            if quantity.isnumeric():
+                quantity = int(quantity)
+                if quantity > 0:
+                    book['quantity'] += quantity
+                    break
+                else:
+                    erro = f'a quantidade "{quantity}" precisa ser maior que 0'
+                    log_erro(erro, username)
+            else:
+                erro = f'a quantidade "{quantity}" não é um número inteiro'
+                log_erro(erro, username)
+    else:
+        book = {}
+        book['title'] = booktitle
+        draw_screen(title, infos, erro = erro)
+        author = input('\n AUTOR: ')
+        author = author.title()
+        book['author'] = author
+        while True:
+            draw_screen(title, infos, erro = erro)
+            quantity = input('\n QUANTIDADE: ')
+            if quantity.isnumeric():
+                quantity = int(quantity)
+                if quantity > 0:
+                    break
+                else:
+                    erro = f'a quantidade "{quantity}" precisa ser maior que 0'
+                    log_erro(erro, username)
+            else:
+                erro = f'a quantidade "{quantity}" não é um número inteiro'
+                log_erro(erro, username)
+        book['quantity'] = quantity
+    # SET:
+    add_book(book)
+    log(f'o livro "{booktitle}" foi adicionado', username)
+
+def remove_books(user:dict) -> None:
+    # SCREEN:
+    title = 'remover livros'
+    infos = 'digite o titulo do livro e a quantidade para remover do acervo de livros'
+    erro = None
+    book = None
+    while True:
+        draw_screen(title, infos, erro = erro, dictionarys = book)
+        # BOOK:
+        booktitle = input('\n LIVRO: ')
+        book = get_book(booktitle)
+        break
+
+def return_reserved_book(user:dict) -> dict:
+    # SCREEN:
+    title = 'devolver livro reservado'
+    infos = 'digite o nome do usuário e o titulo do livro para realizar a devolução ao acervo de livros'
+    erro = None
+    while True:
+        draw_screen(title, infos, erro = erro)
+        break
+    return user
+
+def list_books(user:dict) -> None:
+    title = 'listar livros'
+    options = [
+        ''
+    ]
+    erro = None
+    books = None
+    while True:
+        draw_screen(title, options = options, erro = erro, dictionarys = books)
+        break
 
 def manage_books(user:dict) -> dict:
     username = user['username']
@@ -10,7 +97,7 @@ def manage_books(user:dict) -> dict:
         'voltar',
         'adicionar livros',
         'remover livros',
-        'devolver reserva',
+        'devolver livro reservado',
         'listar livros'
     ]
     erro = None
@@ -27,11 +114,11 @@ def manage_books(user:dict) -> dict:
         if choiced == 1:
             break
         elif choiced == 2:
-            pass
+            add_books(user)
         elif choiced == 3:
-            pass
+            remove_books(user)
         elif choiced == 4:
-            pass
+            user = return_reserved_book(user)
         elif choiced == 5:
-            pass
+            list_books(user)
     return user
